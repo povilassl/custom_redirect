d3add0046d54681e932c824b529d963963027cf5
Povilas Slivinskas <78279117+povilassl@users.noreply.github.com>
2024-02-05 20:03:07 +0300

    add environment settings (#1)

--- background.js
+++ background.js
@@ -1,43 +1,51 @@
-let redirect_from = [
-  "https://www.reddit.com/",
-  "https://www.reddit.com/r/memes/",
-];
+let redirect_rules = {};
 
-let wiki_redirect_to = "https://en.wikipedia.org/wiki/Special:Random";
+fetch(chrome.runtime.getURL("settings.json"))
+  .then((response) => response.json())
+  .then((data) => {
+    redirect_rules = data;
 
-let facebook_redirect_from = "https://www.facebook.com/";
-let facebook_redirect_to = "https://www.facebook.com/messages/";
+    let {
+      redirect_from,
+      wiki_redirect_to,
+      facebook_redirect_from,
+      facebook_redirect_to,
+      freedium_redirect_from,
+      freedium_redirect_to,
+    } = redirect_rules;
 
-let freedium_redirect_from = [
-  "https://medium.com/",
-  "https://towardsdatascience.com/",
-];
+    chrome.webNavigation.onBeforeNavigate.addListener(function (details) {
+      let current_url = details.url.toLowerCase();
 
-let freedium_redirect_to = "https://freedium.cfd/";
+      // fb redirect to messages
+      if (url_equal(facebook_redirect_from, current_url)) {
+        redirect(details, facebook_redirect_to);
+      }
 
-chrome.webNavigation.onBeforeNavigate.addListener(function (details) {
-  let current_url = details.url.toLowerCase();
+      // medium and towards data science redirect to freedium
+      if (
+        freedium_redirect_from.some(
+          (url) => current_url.startsWith(url) && current_url != url
+        )
+      ) {
+        let freedium_url = freedium_redirect_to + "/" + details.url;
+        redirect(details, freedium_url);
+      }
 
-  //fb redirect to messages
-  if (facebook_redirect_from === current_url) {
-    redirect(details, facebook_redirect_to);
-  }
+      // others redirect to wiki
+      if (redirect_from.some((url) => url_equal(current_url, url))) {
+        redirect(details, wiki_redirect_to);
+      }
+    });
+  })
+  .catch((error) => console.error("Error fetching redirect rules:", error));
 
-  //medium and towards data science redirect to freedium
-  if (
-    freedium_redirect_from.some(
-      (url) => current_url.startsWith(url) && current_url != url
-    )
-  ) {
-    let freedium_url = freedium_redirect_to + "/" + details.url;
-    redirect(details, freedium_url);
-  }
+function url_equal(src, trg) {
+  let src_trimmed = src.replace(/\/$/, "");
+  let trg_trimmed = trg.replace(/\/$/, "");
 
-  //others redirect to wiki
-  if (redirect_from.some((url) => current_url === url)) {
-    redirect(details, wiki_redirect_to);
-  }
-});
+  return src_trimmed === trg_trimmed;
+}
 
 function redirect(details, new_url) {
   chrome.tabs.update(details.tabId, { url: new_url });
