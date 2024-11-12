b8fe07904c07cd8a722e013ae1b94446bb7e30bd
povilassl <slivinskas.povilas@gmail.com>
2024-11-12 20:06:18 +0200

    refactor code

--- background.js
+++ background.js
@@ -5,47 +5,35 @@ fetch(chrome.runtime.getURL("settings.json"))
   .then((data) => {
     redirect_rules = data;
 
-    let {
-      redirect_from,
-      wiki_redirect_to,
-      facebook_redirect_from,
-      facebook_redirect_to,
-      freedium_redirect_from,
-      freedium_redirect_to,
-    } = redirect_rules;
+    let { full_redirect, partial_redirect } = redirect_rules;
 
     chrome.webNavigation.onBeforeNavigate.addListener(function (details) {
-      let current_url = details.url.toLowerCase();
+      let current_url = new URL(details.url.toLowerCase());
+      let hostname = current_url.hostname;
 
-      // fb redirect to messages
-      if (url_equal(facebook_redirect_from, current_url)) {
-        redirect(details, facebook_redirect_to);
+      let normalized_hostname =
+        hostname && !hostname.startsWith("www.") ? `www.${hostname}` : hostname;
+
+      let full_redirect_to = full_redirect[normalized_hostname];
+      if (full_redirect_to && current_url != full_redirect_to) {
+        redirect(details, full_redirect_to);
+        return;
       }
 
-      // medium and towards data science redirect to freedium
-      if (
-        freedium_redirect_from.some(
-          (url) => current_url.startsWith(url) && current_url != url
-        )
-      ) {
-        let freedium_url = freedium_redirect_to + "/" + details.url;
-        redirect(details, freedium_url);
+      let partial_redirect_to = partial_redirect[normalized_hostname];
+
+      if (!partial_redirect_to) {
+        return;
       }
 
-      // others redirect to wiki
-      if (redirect_from.some((url) => url_equal(current_url, url))) {
-        redirect(details, wiki_redirect_to);
+      partial_redirect_to += current_url.pathname + current_url.search;
+
+      if (partial_redirect_to && current_url != partial_redirect_to) {
+        redirect(details, partial_redirect_to);
       }
     });
   })
   .catch((error) => console.error("Error fetching redirect rules:", error));
-
-function url_equal(src, trg) {
-  let src_trimmed = src.replace(/\/$/, "");
-  let trg_trimmed = trg.replace(/\/$/, "");
-
-  return src_trimmed === trg_trimmed;
-}
 
 function redirect(details, new_url) {
   chrome.tabs.update(details.tabId, { url: new_url });
